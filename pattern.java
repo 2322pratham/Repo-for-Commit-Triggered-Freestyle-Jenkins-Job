84dfa34c527fe26a1a2af351dae8d55736407071
PRATHAM PRAKASH KAMBLE <mca.2322@unigoa.ac.in>
2024-10-17 15:51:01 +0530

    Update pattern.java

--- pattern.java
+++ pattern.java
@@ -2,5 +2,6 @@ public class pattern {
     public static void main(String[] args) {
         System.out.println("Pattern Java File Executed!");
         System.out.println("Pattern Java File Executed!");
+        System.out.println("Pattern Java File Executed!");
     }
 }
